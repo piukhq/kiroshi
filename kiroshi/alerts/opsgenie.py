31451194afa9c241e624103230b6119d095660ca
nathancread93 <nathancread@outlook.com>
2023-03-08 04:47:09 +0000

    adding ci, dockerfile, opsgenie alert, setting traffic check alert

--- kiroshi/alerts/opsgenie.py
+++ kiroshi/alerts/opsgenie.py
@@ -1,8 +1,12 @@
 from kiroshi.settings import settings
 
 import requests
+import json
 
 def opsgenie(message):
-    request = requests.post(settings.opsgenie_url, headers={'Authorization': f'GenieKey {settings.opsgenie_api_key}'}, data=message)
+    data = json.dumps({"message": message})
+    headers = {'Authorization': f'GenieKey {settings.opsgenie_api_key}', 'Content-Type': 'application/json'}
+    request = requests.post(settings.opsgenie_url, headers=headers, data=data)
     request.raise_for_status()
+    print(request.text)
     return None
