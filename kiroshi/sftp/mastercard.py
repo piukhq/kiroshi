d0a85824fe05fd1cc66a6c660b7091c2cd40021b
backwardspy <backwardspy@pigeon.life>
2023-06-22 16:11:42 +0100

    feat: split mastercard files into settle & refund

--- kiroshi/sftp/mastercard.py
+++ kiroshi/sftp/mastercard.py
@@ -1,8 +1,12 @@
+import io
 from pathlib import Path
+from weakref import ref
 
 import paramiko
 
 from kiroshi.settings import logger
+
+AMOUNT_FIELD = slice(518, 518 + 12)
 
 
 class MastercardSFTP:
@@ -34,10 +38,44 @@ class MastercardSFTP:
         )
         return ssh.open_sftp()
 
+    def split_copy_file(self, fo: io.BytesIO, settlement_path: Path, refund_path: Path) -> None:
+        content = io.TextIOWrapper(fo, encoding="utf-8")
+
+        with settlement_path.open("w") as settlement, refund_path.open("w") as refund:
+            for line in content:
+                if line[0] != "D":
+                    # non-data records get written to both files
+                    settlement.write(line)
+                    refund.write(line)
+                else:
+                    # data records get written to the appropriate file based on the spend amount
+                    spend_amount = int(line[AMOUNT_FIELD])
+                    file = settlement if spend_amount >= 0 else refund
+                    file.write(line)
+
     def run(self) -> None:
         client = self.client()
-        logger.info("Creating Local Directory", directory=self.local_path)
-        Path(self.local_path).mkdir(parents=True, exist_ok=True)
+
+        settlement_path = Path(self.local_path)
+        refund_path = settlement_path.parent / "mastercard-refund/"
+
+        logger.info("Creating local directories", settlement_directory=settlement_path, refund_directory=refund_path)
+        settlement_path.mkdir(parents=True, exist_ok=True)
+        refund_path.mkdir(parents=True, exist_ok=True)
+
         for file in client.listdir(self.remote_path):
-            logger.info("Copying file", from_dir=self.remote_path, to_dir=self.local_path, file=file)
-            client.get(f"{self.remote_path}/{file}", f"{self.local_path}/{file}")
+            settlement_file = settlement_path / file
+            refund_file = refund_path / file
+
+            logger.info(
+                "Copying file",
+                from_dir=self.remote_path,
+                file=file,
+                settlement_file=settlement_file,
+                refund_file=refund_file,
+            )
+
+            fo = io.BytesIO()
+            client.getfo(Path(self.remote_path) / file, fo)
+            fo.seek(0)
+            self.split_copy_file(fo, settlement_file, refund_file)
