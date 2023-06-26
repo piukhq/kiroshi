72a7a61ee9ff8efde4fbaedcd3fa6779c79bacf2
backwardspy <backwardspy@pigeon.life>
2023-06-26 12:12:05 +0100

    feat: allow writing directly to blob storage for mastercard

--- kiroshi/cli/sftp/mastercard.py
+++ kiroshi/cli/sftp/mastercard.py
@@ -1,8 +1,10 @@
 """Module providing a command-line interface for connecting to the Mastercard SFTP and pulling files locally."""
 
+from pathlib import Path
+
 import click
 
-from kiroshi.sftp.mastercard import MastercardSFTP
+from kiroshi.sftp.mastercard import BlobFileWriter, LocalFileWriter, MastercardSFTP
 
 
 @click.command(name="mastercard")
@@ -11,7 +13,12 @@ from kiroshi.sftp.mastercard import MastercardSFTP
     "--host",
     default="files.mastercard.com",
     type=click.Choice(
-        ["files.mastercard.com", "mtf.files.mastercard.com", "uksouthstagingsftpc75y.blob.core.windows.net"],
+        [
+            "files.mastercard.com",
+            "mtf.files.mastercard.com",
+            "uksouthstagingsftpc75y.blob.core.windows.net",
+            "localhost",
+        ],
     ),
     help="The Mastercard SFTP host to connect to",
 )
@@ -19,7 +26,7 @@ from kiroshi.sftp.mastercard import MastercardSFTP
     "-u",
     "--user",
     default="Z216458",
-    type=click.Choice(["Z218502", "Z216458", "Z218502", "uksouthstagingsftpc75y.mastercard.kiroshi"]),
+    type=click.Choice(["Z218502", "Z216458", "Z218502", "uksouthstagingsftpc75y.mastercard.kiroshi", "foo"]),
     help="The Mastercard SFTP user to use",
 )
 @click.option("-p", "--port", default=16022, type=int, help="The Mastercard SFTP port")
@@ -39,14 +46,46 @@ from kiroshi.sftp.mastercard import MastercardSFTP
             "/0073185/production/download/TGX2:/mnt/harmonia-imports/mastercard",
             "/0073185/production/download/TS44:/mnt/harmonia-imports/payment/mastercard",
             "/0073185/test/download/TGX4:/mnt/harmonia-imports/test/mastercard-settlement",
+            "/share:/mnt/harmonia-imports/mastercard",
         ],
     ),
     help="Map of remote and local directories to use, colon seperated",
 )
-def mastercard(host: str, user: str, port: int, keypath: str, directories: str) -> None:
+@click.option(
+    "--use-blob-storage",
+    default=False,
+    is_flag=True,
+    help="Use Azure Blob Storage to store files instead of writing locally",
+)
+def mastercard(  # noqa: PLR0913
+    host: str,
+    user: str,
+    port: int,
+    keypath: str,
+    directories: str,
+    use_blob_storage: bool,  # noqa: FBT001
+) -> None:
     """Connect to the Mastercard SFTP and pull files locally.
 
     Designed to be used in conjunction with Harmonia.
     """
-    sftp = MastercardSFTP(host=host, port=port, user=user, keypath=keypath, directories=directories)
+    remote_path, local_path = map(Path, directories.split(":"))
+
+    container_name = "harmonia-imports"
+    base_path = Path("/mnt") / container_name
+    settlement_path = local_path.relative_to(base_path)
+    refund_path = settlement_path.parent / "mastercard-refund"
+
+    writer = BlobFileWriter(container_name) if use_blob_storage else LocalFileWriter(base_path)
+
+    sftp = MastercardSFTP(
+        host=host,
+        port=port,
+        user=user,
+        keypath=keypath,
+        remote_path=remote_path,
+        writer=writer,
+        settlement_path=settlement_path,
+        refund_path=refund_path,
+    )
     sftp.run()
--- kiroshi/sftp/mastercard.py
+++ kiroshi/sftp/mastercard.py
@@ -1,19 +1,119 @@
 """Module providing a class for interacting with Mastercard SFTP."""
 
 import io
+from abc import ABC, abstractmethod
+from collections.abc import Generator
+from contextlib import ExitStack, contextmanager
 from pathlib import Path
 
 import paramiko
+from azure.storage.blob import BlobServiceClient
 
-from kiroshi.settings import logger
+from kiroshi.settings import logger, settings
 
 AMOUNT_FIELD = slice(518, 518 + 12)
+
+
+FileWriterContext = Generator[tuple[io.TextIOWrapper, ...], None, None]
+
+
+class FileWriter(ABC):
+    """Abstract base class for file writers."""
+
+    @abstractmethod
+    @contextmanager
+    def __call__(self, *paths: Path) -> FileWriterContext:
+        """Open the writer on the given paths.
+
+        Args:
+            paths (Path): The paths to write to.
+
+        Returns:
+            FileWriterContext: A context manager that yields a tuple of file-like objects.
+        """
+
+
+class LocalFileWriter(FileWriter):
+    """FileWriter for local paths."""
+
+    def __init__(self, directory: Path) -> None:
+        """Initialize the class with the given path.
+
+        Args:
+            directory (Path): The direcory to write files into.
+
+        Returns:
+            None
+        """
+        self.directory = directory
+
+    @contextmanager
+    def __call__(self, *paths: Path) -> FileWriterContext:
+        """Open the writer on the given paths.
+
+        Args:
+            paths (Path): The paths to write to.
+
+        Returns:
+            FileWriterContext: A context manager that yields a tuple of file-like objects.
+        """
+        paths = tuple(self.directory / path for path in paths)
+        for path in paths:
+            path.parent.mkdir(parents=True, exist_ok=True)
+
+        with ExitStack() as stack:
+            yield tuple(stack.enter_context(path.open("w")) for path in paths)
+
+
+class BlobFileWriter(FileWriter):
+    """FileWriter for Azure Blob Storage."""
+
+    def __init__(self, container_name: str) -> None:
+        """Initialize the class with the given path.
+
+        Args:
+            container_name (str): The name of the container to write files into.
+        """
+        if not settings.blob_storage_account_dsn:
+            msg = "No blob storage account DSN provided"
+            raise ValueError(msg)
+
+        self.client = BlobServiceClient.from_connection_string(settings.blob_storage_account_dsn)
+        self.container_name = container_name
+
+    @contextmanager
+    def __call__(self, *paths: Path) -> FileWriterContext:
+        """Open the writer on the given paths.
+
+        Args:
+            paths (Path): The paths to write to.
+
+        Returns:
+            FileWriterContext: A context manager that yields a tuple of file-like objects.
+        """
+        bufs = tuple(io.StringIO() for _ in paths)
+        yield bufs
+
+        container_client = self.client.get_container_client(self.container_name)
+        for path, buf in zip(paths, bufs, strict=True):
+            blob_client = container_client.get_blob_client(str(path))
+            blob_client.upload_blob(buf.getvalue().encode("utf-8"))
 
 
 class MastercardSFTP:
     """Class for interacting with Mastercard SFTP."""
 
-    def __init__(self, host: str, port: int, user: str, keypath: str, directories: str) -> None:  # noqa: PLR0913
+    def __init__(  # noqa: PLR0913
+        self,
+        host: str,
+        port: int,
+        user: str,
+        keypath: str,
+        remote_path: Path,
+        writer: FileWriter,
+        settlement_path: Path,
+        refund_path: Path,
+    ) -> None:
         """Initialize the MastercardSFTP class.
 
         Args:
@@ -21,16 +121,19 @@ class MastercardSFTP:
             port (int): The port number of the SFTP server.
             user (str): The username to use when connecting to the SFTP server.
             keypath (str): The path to the private key file to use when connecting to the SFTP server.
-            directories (str): A string containing the remote and local directories to copy files from/to, separated by a colon.
-
-        Returns:
-            None
+            remote_path (Path): The path to the remote directory to use.
+            writer (FileWriter): The FileWriter to use when writing files.
+            settlement_path (Path): The settlement file path relative to writer.
+            refund_path (Path): The refund file path relative to writer.
         """
         self.host = host
         self.port = port
         self.user = user
         self.keypath = keypath
-        self.remote_path, self.local_path = directories.split(":")
+        self.remote_path = remote_path
+        self.writer = writer
+        self.settlement_path = settlement_path
+        self.refund_path = refund_path
 
     def _connect(self) -> paramiko.SFTPClient:
         logger.info(
@@ -56,7 +159,7 @@ class MastercardSFTP:
     def _split_copy_file(self, fo: io.BytesIO, settlement_path: Path, refund_path: Path) -> None:
         content = io.TextIOWrapper(fo, encoding="utf-8")
 
-        with settlement_path.open("w") as settlement, refund_path.open("w") as refund:
+        with self.writer(settlement_path, refund_path) as (settlement, refund):
             for line in content:
                 if line[0] != "D":
                     # non-data records get written to both files
@@ -69,23 +172,12 @@ class MastercardSFTP:
                     file.write(line)
 
     def run(self) -> None:
-        """Run the SFTP client to copy files from the remote server to the local machine.
-
-        Returns
-            None
-        """
+        """Run the SFTP client to copy files from the remote server to the local machine."""
         client = self._connect()
 
-        settlement_path = Path(self.local_path)
-        refund_path = settlement_path.parent / "mastercard-refund/"
-
-        logger.info("Creating local directories", settlement_directory=settlement_path, refund_directory=refund_path)
-        settlement_path.mkdir(parents=True, exist_ok=True)
-        refund_path.mkdir(parents=True, exist_ok=True)
-
-        for file in client.listdir(self.remote_path):
-            settlement_file = settlement_path / file
-            refund_file = refund_path / file
+        for file in client.listdir(str(self.remote_path)):
+            settlement_file = self.settlement_path / file
+            refund_file = self.refund_path / file
 
             logger.info(
                 "Copying file",
@@ -96,6 +188,6 @@ class MastercardSFTP:
             )
 
             fo = io.BytesIO()
-            client.getfo(Path(self.remote_path) / file, fo)
+            client.getfo(str(self.remote_path / file), fo)
             fo.seek(0)
             self._split_copy_file(fo, settlement_file, refund_file)
