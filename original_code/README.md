6094e4af5722043e121cd243b8c005dccac30902
Chris Pressland <mail@cpressland.io>
2023-07-03 18:29:20 +0100

    Remove 'original_code'

--- original_code/README.md
+++
@@ -1,2 +0,0 @@
-# kiroshi
-Monitors the Monitoring
--- original_code/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-prometheus_url = os.getenv('', 'http://prometheus.prometheus:9090')
-teams_webhook = os.environ["teams_webhook"]
-opsgenie_url = os.environ[""]
-opsgenie_api_key = os.environ[""]
