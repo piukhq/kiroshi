ac9bcbcd5abd3c29736255a171fbef147cd64962
Francesco Milani <fmilani@bink.com>
2023-07-26 14:03:25 +0300

    update sqlalchemy models with 2.0 syntax

--- kiroshi/database.py
+++ kiroshi/database.py
@@ -6,31 +6,33 @@ It defines the following models:
 
 Each model represents a table in the database and is defined using SQLAlchemy.
 """
-from sqlalchemy import ARRAY, Column, DateTime, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+from sqlalchemy import ARRAY, String, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, declarative_base, mapped_column
 
 from kiroshi.settings import settings
 
 engine = create_engine(settings.database_dsn, echo=False)
-base = declarative_base()
+Base: DeclarativeBase = declarative_base()
 
 
-class FrontDoorRanges(base):
+class FrontDoorRanges(Base):
     """Model representing the front door ranges table in the database."""
 
     __tablename__ = "frontdoor_ranges"
 
-    id = Column(Integer, primary_key=True)  # noqa: A003
-    ranges = Column(ARRAY(String))
-    last_updated = Column(DateTime)
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    ranges: Mapped[list[str] | None] = mapped_column(ARRAY(String))
+    last_updated: Mapped[datetime | None]
 
 
-class FrontDoorIPs(base):
+class FrontDoorIPs(Base):
     """Model representing the front door IPs table in the database."""
 
     __tablename__ = "frontdoor_ips"
 
-    id = Column(Integer, primary_key=True)  # noqa: A003
-    domain = Column(String)
-    ips = Column(ARRAY(String))
-    last_updated = Column(DateTime)
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    domain: Mapped[str | None]
+    ips: Mapped[list[str] | None] = mapped_column(ARRAY(String))
+    last_updated: Mapped[datetime | None]
