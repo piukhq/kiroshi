5741ab4401be8f03fa2add8118479cee52eef18e
Chris Pressland <mail@cpressland.io>
2023-07-31 11:10:25 +0100

    Linting Fixes

--- kiroshi/alerts/opsgenie.py
+++ kiroshi/alerts/opsgenie.py
@@ -17,10 +17,13 @@ def opsgenie(message: str) -> None:
         requests.exceptions.HTTPError: If the request to OpsGenie fails.
     """
     data = json.dumps({"message": message})
-    api_keys = ["74f3f087-c29e-4490-a410-21c8f24e394c", "3a2778f0-2de5-48bd-b99f-a07d5ac9b211"]
+    api_keys = ("74f3f087-c29e-4490-a410-21c8f24e394c", "3a2778f0-2de5-48bd-b99f-a07d5ac9b211")
     for key in api_keys:
         headers = {"Authorization": f"GenieKey {key}", "Content-Type": "application/json"}
         request = requests.post(
-            "https://api.opsgenie.com/v2/alerts", headers=headers, data=data, timeout=REQUEST_TIMEOUT,
+            "https://api.opsgenie.com/v2/alerts",
+            headers=headers,
+            data=data,
+            timeout=REQUEST_TIMEOUT,
         )
         request.raise_for_status()
