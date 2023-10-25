2f55ea004e4f32a0bb9b10a72e178cb7f060fad1
Chris Pressland <mail@cpressland.io>
2023-10-25 16:40:45 +0100

    Add ability to upgrade linkerd control plane

--- kiroshi/kubernetes/linkerd.py
+++ kiroshi/kubernetes/linkerd.py
@@ -11,13 +11,25 @@ class KubernetesLinkerd:
         """Initialize the KubernetesLinkerd class."""
 
     def check(self) -> None:
-        """Check for the existance of linkerd-proxy sidecars in pods, delete pods without them."""
+        """Check if linkerd annotation is present, if so, check linkerd-proxy is present and at the correct version."""
+        linkerd_version = kr8s.get("pods", namespace="linkerd")[0].spec.containers[0].image.split(":")[1]
         pods = kr8s.get("pods", namespace=kr8s.ALL)
+        logger.info(f"Linkerd version: {linkerd_version}")
         for pod in pods:
-            if "linkerd.io/inject" in pod.annotations and not next(
-                (container for container in pod.spec.containers if container.name == "linkerd-proxy"), None
-            ):
-                logger.info(
-                    f"linkerd missing in pod {pod.metadata.name} in namespace {pod.metadata.namespace}, deleting"
+            logger.info(f"Checking pod {pod.metadata.name} in namespace {pod.metadata.namespace}")
+            if "linkerd.io/inject" in pod.annotations:
+                container = next(
+                    (container for container in pod.spec.containers if container.name == "linkerd-proxy"), None
                 )
-                pod.delete()
+                if not container:
+                    logger.warning(
+                        f"linkerd-proxy not present in pod {pod.metadata.name} in namespace {pod.metadata.namespace}, deleting pod"
+                    )
+                    pod.delete()
+                if container:
+                    pod_linkerd_version = container.image.split(":")[1]
+                    if pod_linkerd_version != linkerd_version:
+                        logger.warning(
+                            f"linkerd-proxy version {pod_linkerd_version} in pod {pod.metadata.name} in namespace {pod.metadata.namespace} does not match linkerd version {linkerd_version}, deleting pod"
+                        )
+                        pod.delete()
